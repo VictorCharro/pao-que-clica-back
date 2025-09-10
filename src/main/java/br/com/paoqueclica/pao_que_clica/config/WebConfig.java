85e9516de39fba93e76a833ba2239ebf67fa5e1b
VcsDevs <v_chs@hotmail.com>
2025-09-10 20:30:05 -0300

    teste

--- src/main/java/br/com/paoqueclica/pao_que_clica/config/WebConfig.java
+++ src/main/java/br/com/paoqueclica/pao_que_clica/config/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Aplica a todos os endpoints
-                .allowedOrigins("*") // Permite o seu frontend
+                .allowedOrigins("*") // Permite qualquer origem
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false); // Mudou para false
     }
 }
