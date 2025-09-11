9b547e1de484a76fce4d4c16a88ba2dcd9523a51
VcsDevs <v_chs@hotmail.com>
2025-09-10 23:15:27 -0300

    teste

--- src/main/java/br/com/paoqueclica/pao_que_clica/controller/PaoController.java
+++ src/main/java/br/com/paoqueclica/pao_que_clica/controller/PaoController.java
@@ -18,73 +18,31 @@ public class PaoController {
 
     @GetMapping("/estado")
     public ResponseEntity<Pao> verificarPao(){
-        try {
-            System.out.println("=== INICIANDO verificarPao() ===");
-            Pao verificado = paoService.verificarPao();
-            System.out.println("=== Pão verificado: " + verificado + " ===");
-            return ResponseEntity.ok(verificado);
-        } catch (Exception e) {
-            System.err.println("=== ERRO em verificarPao(): " + e.getMessage() + " ===");
-            e.printStackTrace();
-            throw e;
-        }
-    }
-
-    @GetMapping("/test")
-    public ResponseEntity<String> test() {
-        return ResponseEntity.ok("Controller funcionando!");
+        Pao verificado = paoService.verificarPao();
+        return ResponseEntity.ok(verificado);
     }
 
     @PostMapping("/clique")
     public ResponseEntity<Pao> clique(){
-        try {
-            System.out.println("=== INICIANDO clique() ===");
-            Pao atualizado = paoService.clique();
-            System.out.println("=== Pão atualizado: " + atualizado + " ===");
-            return ResponseEntity.ok(atualizado);
-        } catch (Exception e) {
-            System.err.println("=== ERRO em clique(): " + e.getMessage() + " ===");
-            e.printStackTrace();
-            throw e;
-        }
+        Pao atualizado = paoService.clique();
+        return ResponseEntity.ok(atualizado);
     }
 
     @PostMapping("/upgrade1")
     public ResponseEntity<Pao> upgrade1(){
-        try {
-            System.out.println("=== INICIANDO upgrade1() ===");
-            Pao atualizado = paoService.upgrade1();
-            return ResponseEntity.ok(atualizado);
-        } catch (Exception e) {
-            System.err.println("=== ERRO em upgrade1(): " + e.getMessage() + " ===");
-            e.printStackTrace();
-            throw e;
-        }
+        Pao atualizado = paoService.upgrade1();
+        return ResponseEntity.ok(atualizado);
     }
 
     @PostMapping("/cliqueAutomatico")
     public ResponseEntity<Pao> cliqueAutomatico(){
-        try {
-            System.out.println("=== INICIANDO cliqueAutomatico() ===");
-            Pao atualizado = paoService.cliqueAutomatico();
-            return ResponseEntity.ok(atualizado);
-        } catch (Exception e) {
-            System.err.println("=== ERRO em cliqueAutomatico(): " + e.getMessage() + " ===");
-            e.printStackTrace();
-            throw e;
-        }
+        Pao atualizado = paoService.cliqueAutomatico();
+        return ResponseEntity.ok(atualizado);
     }
 
     @PostMapping("/reset")
     public ResponseEntity<Pao> reset(){
-        try {
-            System.out.println("=== INICIANDO reset() ===");
-            Pao atualizado = paoService.reset();
-            return ResponseEntity.ok(atualizado);
-        } catch (Exception e) {
-            System.err.println("=== ERRO em reset(): " + e.getMessage() + " ===");
-            e.printStackTrace();
-            throw e;
-        }
+        Pao atualizado = paoService.reset();
+        return ResponseEntity.ok(atualizado);
     }
 }
